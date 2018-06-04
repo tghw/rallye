47bab444edbd609a4d415f5544edd55a85f127a4
Tyler G. Hicks-Wright <ty@tghw.com>
2018-06-04 18:23:18 +0000

    Auto update time.

---
+++ boot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo -u www-data /home/pi/code/rallye/venv/bin/python /home/pi/code/rallye/updater.py start
+
--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cd /home/pi/code/rallye
+git reset --hard
+git pull --rebase origin master
 touch /etc/uwsgi-emperor/vassals/rallye.ini
 /home/pi/code/rallye/venv/bin/python /home/pi/code/rallye/updater.py restart
 
