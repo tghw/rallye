9c3ce70ec870bfc18a4b2f82d94fe67c755238bc
Tyler G. Hicks-Wright <ty@tghw.com>
2018-06-08 22:50:22 -0600

    ahhhhhhhhhhhhH!

--- client/src/dashboard.vue
+++ client/src/dashboard.vue
@@ -319,6 +319,7 @@ export default {
         update() {
             this.$http.get('/update').then((resp) => {
                 this.leg = resp.body.leg;
+                this.last_leg = resp.body.leg;
                 this.cast = resp.body.cast;
                 this.error = resp.body.error;
                 this.time = resp.body.time;
